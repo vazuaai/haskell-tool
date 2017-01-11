967a405ef76dd7595ccecc599d17b3dfe79672a0
Zoltan Varju Vilmos <zoltn.varju@gmail.com>
2017-01-11 17:19:40 +0100

    new function

---
+++ KeepAliveCommand.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python35
+
+import sublime
+import sublime_plugin
+import os
+import socket
+from threading import Thread
+import subprocess
+import time
+import errno 
+import json
+import time
+
+from .client.ClientManager import ClientManager
+
+class KeepAliveCommand(sublime_plugin.ApplicationCommand):
+
+	def run(self):
+		ClientManager._instance.keep_alive()
